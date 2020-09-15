77099fc103e4e7751415018cb2b01d57c9676811
trieu92dmt <69976199+trieu92dmt@users.noreply.github.com>
2020-09-15 15:58:20 +0700

    Update script.js

--- js/script.js
+++ js/script.js
@@ -34,7 +34,7 @@ $(document).ready(function(){
                 a.style.top=0;
                 a.style.left=0;
             })
-        },3);
+        },1);
         $("*").keydown(function(e){
             if (left< area.clientWidth-target.clientWidth && e.keyCode==83)
             {
